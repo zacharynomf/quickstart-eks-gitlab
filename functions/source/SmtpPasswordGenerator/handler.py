7402b4b815ad945c493ca1c439a770dbf7d5b4e6
Dmitry Kolomiets <kolomied@amazon.com>
2020-12-08 19:16:02 +0000

    SMTP support, entry templates cleanup

---
+++ functions/source/SmtpPasswordGenerator/handler.py
@@ -0,0 +1,51 @@
+from __future__ import print_function
+from crhelper import CfnResource
+import logging
+import hmac
+import hashlib
+import base64
+
+# Values that are required to calculate the signature. These values should
+# never change.
+DATE = "11111111"
+SERVICE = "ses"
+MESSAGE = "SendRawEmail"
+TERMINAL = "aws4_request"
+VERSION = 0x04
+
+logger = logging.getLogger(__name__)
+# Initialise the helper, all inputs are optional, this example shows the defaults
+helper = CfnResource(json_logging=False, log_level='DEBUG', boto_level='CRITICAL', sleep_on_delete=120)
+
+@helper.create
+@helper.update
+def create(event, context):
+    logger.info("Got Create")
+    region = event['ResourceProperties']['Region']
+    secret = event['ResourceProperties']['Secret']
+
+    logger.info(f"Calculating password for{region}")
+    smtp_password = calculateKey(secret, region)
+    helper.Data.update({"Password": smtp_password})
+
+@helper.delete
+def delete(event, context):
+    logger.info("Got Delete")
+
+def handler(event, context):
+    helper(event, context)
+
+def sign(key, msg):
+    return hmac.new(key, msg.encode('utf-8'), hashlib.sha256).digest()
+
+# https://docs.aws.amazon.com/ses/latest/DeveloperGuide/smtp-credentials.html#smtp-credentials-convert
+def calculateKey(secretAccessKey, region):
+    signature = sign(("AWS4" + secretAccessKey).encode('utf-8'), DATE)
+    signature = sign(signature, region)
+    signature = sign(signature, SERVICE)
+    signature = sign(signature, TERMINAL)
+    signature = sign(signature, MESSAGE)
+    signatureAndVersion = bytes([VERSION]) + signature
+    smtpPassword = base64.b64encode(signatureAndVersion)
+    
+    return smtpPassword.decode('utf-8')
