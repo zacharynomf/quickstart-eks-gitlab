3fd484cb88c6a5e0b756c8a827f32910060858fe
Dmitry Kolomiets <kolomied@amazon.com>
2021-07-27 15:14:23 +0100

    Fix ACM cert cleanup lambda

--- functions/source/CleanupACMDNSValidation/handler.py
+++ functions/source/CleanupACMDNSValidation/handler.py
@@ -2,15 +2,9 @@ import json
 import logging
 import boto3
 import cfnresponse
-import time
-import re
-
 
 acm_client = boto3.client('acm')
 r53_client = boto3.client('route53')
-lambda_client = boto3.client('lambda')
-logs_client = boto3.client('logs')
-
 
 def handler(event, context):
     print('Received event: %s' % json.dumps(event))
@@ -18,7 +12,9 @@ def handler(event, context):
     physical_resource_id = None
     data = {}
     reason = None
+
     try:
+
         if event['RequestType'] == 'Create':
             token = ''.join(ch for ch in str(event['StackId'] + event['LogicalResourceId']) if ch.isalnum())
             token = token[len(token) - 32:]
@@ -28,34 +24,39 @@ def handler(event, context):
             # doesn't do anything on create
             physical_resource_id = event['PhysicalResourceId']
         elif event['RequestType'] == 'Delete':
-            physical_resource_id = event['PhysicalResourceId']
             acm_arn = event["ResourceProperties"]["ACMArn"]
-            rs = {}
+            hosted_zone_id = event['ResourceProperties']['HostedZoneId']
+            physical_resource_id = event['PhysicalResourceId']
+
+            changes = []
             for d in acm_client.describe_certificate(CertificateArn=acm_arn)['Certificate']['DomainValidationOptions']:
-                rs[d['ResourceRecord']['Name']] = d['ResourceRecord']['Value']
-            rs = [{'Action': 'DELETE', 'ResourceRecordSet': {'Name': r, 'Type': 'CNAME', 'TTL': 600,'ResourceRecords': [{'Value': rs[r]}]}} for r in rs.keys()]
-            try:
-                r53_client.change_resource_record_sets(HostedZoneId=event['ResourceProperties']['HostedZoneId'], ChangeBatch={'Changes': rs})
-            except r53_client.exceptions.InvalidChangeBatch as e:
-                pass
-            time.sleep(30)
+                record_name = d['ResourceRecord']['Name']
+                record_type = d['ResourceRecord']['Type']
+
+                # Retreiving information about the record (we need TTL in particular)
+                response = r53_client.list_resource_record_sets(
+                    HostedZoneId=hosted_zone_id,
+                    StartRecordName=record_name,
+                    StartRecordType=record_type
+                )
+                
+                record = next(filter(lambda x: x['Name'] == record_name, response['ResourceRecordSets']))
+
+                changes.append({
+                    'Action': 'DELETE', 
+                    'ResourceRecordSet': {
+                        'Name': record_name, 
+                        'Type': record_type, 
+                        'TTL': record['TTL'], 
+                        'ResourceRecords': record['ResourceRecords']
+                    }
+                })
+
+            r53_client.change_resource_record_sets(HostedZoneId=hosted_zone_id, ChangeBatch={'Changes': changes})
 
     except Exception as e:
         logging.error('Exception: %s' % e, exc_info=True)
         reason = str(e)
         status = cfnresponse.FAILED
     finally:
-        if event['RequestType'] == 'Delete':
-            try:
-                wait_message = 'waiting for events for request_id %s to propagate to cloudwatch...' % context.aws_request_id
-                while not logs_client.filter_log_events(
-                        logGroupName=context.log_group_name,
-                        logStreamNames=[context.log_stream_name],
-                        filterPattern='"%s"' % wait_message
-                )['events']:
-                    print(wait_message)
-                    time.sleep(5)
-            except Exception as e:
-                logging.error('Exception: %s' % e, exc_info=True)
-                time.sleep(120)
         cfnresponse.send(event, context, status, data, physical_resource_id, reason)
