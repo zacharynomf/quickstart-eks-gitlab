2c7fbdeb9b4828e34d10e5ff25e53ce55d6ff2a7
Troy Lindsay <tlindsay42@users.noreply.github.com>
2023-02-18 21:01:05 -0500

    Update SMTP password generator
    * Rename handler
    * Use ECR Public Gallery SAM python 3.9 build image
    * Use crhelper layer from upstream EKS stack rather than package old
      version
    * Move function below role to match deploy sequence

--- functions/source/SmtpPasswordGenerator/index.py
+++ functions/source/SmtpPasswordGenerator/index.py
@@ -1,9 +1,10 @@
 from __future__ import print_function
-from crhelper import CfnResource
-import logging
-import hmac
-import hashlib
 import base64
+import hashlib
+import hmac
+import json
+import logging
+from crhelper import CfnResource
 
 # Values that are required to calculate the signature. These values should
 # never change.
@@ -15,37 +16,49 @@ VERSION = 0x04
 
 logger = logging.getLogger(__name__)
 # Initialise the helper, all inputs are optional, this example shows the defaults
-helper = CfnResource(json_logging=False, log_level='DEBUG', boto_level='CRITICAL', sleep_on_delete=120)
+helper = CfnResource(
+    json_logging=False, log_level="DEBUG", boto_level="CRITICAL", sleep_on_delete=120
+)
+
 
 @helper.create
 @helper.update
-def create(event, context):
+def create(event, _):
     logger.info("Got Create")
-    region = event['ResourceProperties']['Region']
-    secret = event['ResourceProperties']['Secret']
+    region = event["ResourceProperties"]["Region"]
+    secret = event["ResourceProperties"]["Secret"]
 
     logger.info(f"Calculating password for{region}")
     smtp_password = calculateKey(secret, region)
     helper.Data.update({"Password": smtp_password})
 
+
 @helper.delete
-def delete(event, context):
+def delete(_event, _):
     logger.info("Got Delete")
 
+
 def handler(event, context):
+    props = event.get("ResourceProperties", {})
+    logger.setLevel(props.get("LogLevel", logging.INFO))
+
+    logger.debug(json.dumps(event))
+
     helper(event, context)
 
+
 def sign(key, msg):
-    return hmac.new(key, msg.encode('utf-8'), hashlib.sha256).digest()
+    return hmac.new(key, msg.encode("utf-8"), hashlib.sha256).digest()
+
 
 # https://docs.aws.amazon.com/ses/latest/DeveloperGuide/smtp-credentials.html#smtp-credentials-convert
 def calculateKey(secretAccessKey, region):
-    signature = sign(("AWS4" + secretAccessKey).encode('utf-8'), DATE)
+    signature = sign(("AWS4" + secretAccessKey).encode("utf-8"), DATE)
     signature = sign(signature, region)
     signature = sign(signature, SERVICE)
     signature = sign(signature, TERMINAL)
     signature = sign(signature, MESSAGE)
     signatureAndVersion = bytes([VERSION]) + signature
     smtpPassword = base64.b64encode(signatureAndVersion)
-    
-    return smtpPassword.decode('utf-8')
+
+    return smtpPassword.decode("utf-8")
